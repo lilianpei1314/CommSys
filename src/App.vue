07249b5afbca8482662cea13d1dacd2bdc84fb72
lilianpei1314 <lilianpei0710@163.com>
2025-07-30 01:01:53 +0800

    首页（30%）

--- src/App.vue
+++ src/App.vue
@@ -7,5 +7,21 @@
 </template>
 
 <style >
+::-webkit-scrollbar-track{
+  background: raba(0,0,0,0.1);
+  border-radius: 0;
+}
 
+::-webkit-scrollbar{
+  -webkit-appearance: none;
+  width: 10px;
+  height: 10px;
+}
+
+::-webkit-scrollbar-thumb{
+  cursor: pointer;
+  border-radius: 5px;
+  background: rgba(0,0,0,0.25);
+  transform: color 0.2s ease;
+}
 </style>
